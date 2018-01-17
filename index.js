e50ae0cbaeec9cc71f1cbfb5ce2308ccd7b852ec
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-01-18 00:53:28 +0100

    init repro

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var css = require('sheetify')
 var choo = require('choo')
 var store = require('./stores/clicks')
 
-css('tachyons')
+css('./css/main.scss')
 
 var app = choo()
 if (process.env.NODE_ENV !== 'production') {
